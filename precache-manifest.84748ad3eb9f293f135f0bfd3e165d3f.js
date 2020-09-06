bbc131181c6e08af4bc4065a58177c0a11cd51ac
rambabu <rambabu.roges96@gmail.com>
2020-09-06 12:58:04 +0530

    Updates

---
+++ precache-manifest.84748ad3eb9f293f135f0bfd3e165d3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e954158592fbbd276a099e061cf6c9d",
+    "url": "/deploye/index.html"
+  },
+  {
+    "revision": "159324039730d3372d2f",
+    "url": "/deploye/static/css/main.67ec948d.chunk.css"
+  },
+  {
+    "revision": "67a6b61e3c815ed25f6c",
+    "url": "/deploye/static/js/2.180d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploye/static/js/2.180d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "159324039730d3372d2f",
+    "url": "/deploye/static/js/main.80450ca3.chunk.js"
+  },
+  {
+    "revision": "4ded1f4b5e3f77508459",
+    "url": "/deploye/static/js/runtime-main.0d965fe2.js"
+  }
+]);
