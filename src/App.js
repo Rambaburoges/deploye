296ba361f963221d12e547af35cf8cd825f9c942
rambabu <rambabu.roges96@gmail.com>
2020-09-06 12:57:04 +0530

    deploye my react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2> text my react app to deploye in github</h2>
+    </div>
+  );
+}
+
+export default App;
